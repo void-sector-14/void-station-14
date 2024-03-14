f6c041bb8d6ac40158013d9f7e97a20ae76aabfc
Voomra <dimon550@gmail.com>
2024-03-15 02:11:49 +0300

    tweak(tools): pnpm -> npm

--- Czeshuika.Tools/alerts_localization.sh
+++ Czeshuika.Tools/alerts_localization.sh
@@ -5,7 +5,7 @@ SCRIPT_DIR=$(cd "$(dirname "$0")" &>/dev/null && pwd)
 if [[ "$1" == "build" ]]; then
     echo "--- BUILD TOOLS ---"
     cd "$SCRIPT_DIR"/src
-    pnpm run build
+    npm run build
     exit 0
 fi
 
