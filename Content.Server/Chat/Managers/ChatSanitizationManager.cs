6446a193e8cde2b35f34fb8acc74ff16e0fc76bb
Makot <andrey.crat@yandex.ru>
2024-03-19 18:44:01 +0200

    fix: санитайзер

--- Content.Server/Chat/Managers/ChatSanitizationManager.cs
+++ Content.Server/Chat/Managers/ChatSanitizationManager.cs
@@ -12,6 +12,30 @@ public sealed class ChatSanitizationManager : IChatSanitizationManager
     private static readonly Dictionary<string, string> SmileyToEmote = new()
     {
         // I could've done this with regex, but felt it wasn't the right idea.
+		{ "хд", "chatsan-laughs" },
+        { "о-о", "chatsan-wide-eyed" }, // cyrillic о
+        { "о.о", "chatsan-wide-eyed" }, // cyrillic о
+        { "0_о", "chatsan-wide-eyed" }, // cyrillic о
+        { "о/", "chatsan-waves" }, // cyrillic о
+        { "о7", "chatsan-salutes" }, // cyrillic о
+        { "0_o", "chatsan-wide-eyed" },
+        { "лмао", "chatsan-laughs" },
+        { "рофл", "chatsan-laughs" },
+        { "яхз", "chatsan-shrugs" },
+        { ":0", "chatsan-surprised" },
+        { ":р", "chatsan-stick-out-tongue" }, // cyrillic р
+        { "кек", "chatsan-laughs" },
+        { "T_T", "chatsan-cries" },
+        { "Т_Т", "chatsan-cries" }, // cyrillic T
+        { "=_(", "chatsan-cries" },
+        { "!с", "chatsan-laughs" },
+        { "!в", "chatsan-sighs" },
+        { "!х", "chatsan-claps" },
+        { "!щ", "chatsan-snaps" },
+        { "))", "chatsan-smiles-widely" },
+        { ")", "chatsan-smiles" },
+        { "((", "chatsan-frowns-deeply" },
+        { "(", "chatsan-frowns" },
         { ":)", "chatsan-smiles" },
         { ":]", "chatsan-smiles" },
         { "=)", "chatsan-smiles" },
@@ -40,6 +64,14 @@ public sealed class ChatSanitizationManager : IChatSanitizationManager
         { ":<", "chatsan-pouts" },
         { "xD", "chatsan-laughs" },
         { "xд", "chatsan-laughs" }, // cyrillic
+        { ":'(", "chatsan-cries" },
+        { ":'[", "chatsan-cries" },
+        { "='(", "chatsan-cries" },
+        { "='[", "chatsan-cries" },
+        { ")':", "chatsan-cries" },
+        { "]':", "chatsan-cries" },
+        { ")'=", "chatsan-cries" },
+        { "]'=", "chatsan-cries" },
         { ";-;", "chatsan-cries" },
         { ";_;", "chatsan-cries" },
         { "qwq", "chatsan-cries" },
@@ -77,6 +109,19 @@ public sealed class ChatSanitizationManager : IChatSanitizationManager
         { "о7", "chatsan-salutes" }, // cyrillic
         { ";_;7", "chatsan-tearfully-salutes"},
         { "idk", "chatsan-shrugs" },
+        { "idk.", "chatsan-shrugs" },
+        { ";)", "chatsan-winks" },
+        { ";]", "chatsan-winks" },
+        { "(;", "chatsan-winks" },
+        { "[;", "chatsan-winks" },
+        { ":')", "chatsan-tearfully-smiles" },
+        { ":']", "chatsan-tearfully-smiles" },
+        { "=')", "chatsan-tearfully-smiles" },
+        { "=']", "chatsan-tearfully-smiles" },
+        { "(':", "chatsan-tearfully-smiles" },
+        { "[':", "chatsan-tearfully-smiles" },
+        { "('=", "chatsan-tearfully-smiles" },
+        { "['=", "chatsan-tearfully-smiles" },
     };
 
     private bool _doSanitize;
