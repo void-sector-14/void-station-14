34c9ac7beb3e65b49b54cfa5f583758669309215
Makot <andrey.crat@yandex.ru>
2024-08-15 20:08:11 +0300

    fix: исправлено много разных ошибок
    Возможно порезан некий функционал, незабыть проверить

--- Content.Shared/Localizations/ContentLocalizationManager.cs
+++ Content.Shared/Localizations/ContentLocalizationManager.cs
@@ -50,8 +50,6 @@ namespace Content.Shared.Localizations
              */
             var cultureEn = new CultureInfo("en-US");
 
-            _loc.LoadCulture(cultureEn);
-
             _loc.AddFunction(cultureEn, "MAKEPLURAL", FormatMakePlural);
             _loc.AddFunction(cultureEn, "MANY", FormatMany);
         }
