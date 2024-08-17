168bafe46f3d80fe9a5e0fdad552b9345982a704
Malanisa <malanisa@list.ru>
2024-08-17 20:15:48 +0500

    tweak: обновления запуска\сборки (для себя, хз как у вас)

--- runclient.bat
+++ runclient.bat
@@ -1,2 +1,6 @@
 @echo off
+set PDIR=%~dp0
+cd %PDIR%
 dotnet run --project Content.Client
+set PDIR=
+pause
--- runserver.bat
+++ runserver.bat
@@ -1,3 +1,8 @@
 @echo off
+set PDIR=%~dp0
+cd %PDIR%
 dotnet run --project Content.Server
+set PDIR=
 pause
+
+
