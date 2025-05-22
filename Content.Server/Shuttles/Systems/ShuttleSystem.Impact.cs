70b7f5d07ffaf33e8e7c5ad24baa7e6cb1632268
Makot <andrey.crat@yandex.ru>
2025-05-22 17:00:34 +0300

    Revert "add: урон при столкновении гридов"
    This reverts commit 1127431495deb7dd5a9f5d5ad75faa4e90afb388.

--- Content.Server/Shuttles/Systems/ShuttleSystem.Impact.cs
+++ Content.Server/Shuttles/Systems/ShuttleSystem.Impact.cs
@@ -1,38 +1,20 @@
 using System.Numerics;
 using Content.Server.Shuttles.Components;
-using Robust.Server.GameObjects;
 using Content.Shared.Audio;
 using Robust.Shared.Audio;
 using Robust.Shared.Map;
-// using Robust.Shared.Physics.Dynamics; // Horizon deletion
+using Robust.Shared.Physics.Dynamics;
 using Robust.Shared.Physics.Events;
-// using Robust.Shared.Player; // Horizon deletion
-using Robust.Shared.Map.Components;
-using Content.Shared.Damage;
+using Robust.Shared.Player;
 
 namespace Content.Server.Shuttles.Systems;
 
 public sealed partial class ShuttleSystem
 {
-    [Dependency] private readonly MapSystem _mapSys = default!; // Horizon
-    [Dependency] private readonly DamageableSystem _damageSys = default!; // Horizon
-
     /// <summary>
     /// Minimum velocity difference between 2 bodies for a shuttle "impact" to occur.
     /// </summary>
     private const int MinimumImpactVelocity = 10;
-
-    // Horizon start // collision damage
-    /// <summary>
-    /// Kinetic energy required to dismantle a single tile
-    /// </summary>
-    private const float TileBreakEnergy = 10000;
-
-    /// <summary>
-    /// Kinetic energy required to spawn sparks
-    /// </summary>
-    private const float SparkEnergy = 14000;
-    // Horizon end
 
     private readonly SoundCollectionSpecifier _shuttleImpactSound = new("ShuttleImpactSound");
 
@@ -43,13 +25,8 @@ public sealed partial class ShuttleSystem
 
     private void OnShuttleCollide(EntityUid uid, ShuttleComponent component, ref StartCollideEvent args)
     {
-
-        // if (!HasComp<ShuttleComponent>(args.OtherEntity)) // Horizon deletion
-        // Horizon start // change check from "if we're a shuttle" to "both must be grids"
-        if (!TryComp<MapGridComponent>(uid, out var ourGrid) ||
-            !TryComp<MapGridComponent>(args.OtherEntity, out var otherGrid))
+        if (!HasComp<ShuttleComponent>(args.OtherEntity))
             return;
-        // Horizon end
 
         var ourBody = args.OurBody;
         var otherBody = args.OtherBody;
@@ -74,41 +51,10 @@ public sealed partial class ShuttleSystem
             return;
         }
 
-        // Horizon start // ship collisions
-        var energy = ourBody.Mass * Math.Pow(jungleDiff, 2) / 2;
-        var dir = (ourVelocity.Length() > otherVelocity.Length() ? ourVelocity : -otherVelocity).Normalized();
-        ProcessTile(uid, ourGrid, (Vector2i) ourPoint, (float) energy, -dir);
-        ProcessTile(args.OtherEntity, otherGrid, (Vector2i) otherPoint, (float) energy, dir);
-        // Horizon end
-
         var coordinates = new EntityCoordinates(ourXform.MapUid.Value, args.WorldPoint);
         var volume = MathF.Min(10f, 1f * MathF.Pow(jungleDiff, 0.5f) - 5f);
         var audioParams = AudioParams.Default.WithVariation(SharedContentAudioSystem.DefaultVariation).WithVolume(volume);
 
         _audio.PlayPvs(_shuttleImpactSound, coordinates, audioParams);
     }
-
-    // Horizon start // function to destroy tiles
-    private void ProcessTile(EntityUid uid, MapGridComponent grid, Vector2i tile, float energy, Vector2 dir)
-    {
-        DamageSpecifier damage = new();
-        damage.DamageDict = new() { { "Blunt", energy } };
-
-        foreach (EntityUid localUid in _lookup.GetLocalEntitiesIntersecting(uid, tile, gridComp: grid))
-        {
-            _damageSys.TryChangeDamage(localUid, damage);
-
-            TransformComponent form = Transform(localUid);
-            if (!form.Anchored)
-                _transform.Unanchor(localUid, form);
-            _throwing.TryThrow(localUid, dir);
-        }
-
-        if (energy > TileBreakEnergy)
-            _mapSys.SetTile(new Entity<MapGridComponent>(uid, grid), tile, Tile.Empty);
-
-        if (energy > SparkEnergy)
-            SpawnAtPosition("EffectSparks", new EntityCoordinates(uid, tile));
-    }
-    // Horizon end
 }
